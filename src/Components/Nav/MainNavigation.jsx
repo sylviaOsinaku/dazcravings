92f5b206e4c38b807767b2ca5890c4b9107af410
sylviaOsinaku <osinakusylvia@gmail.com>
2023-03-18 02:48:38 +0100

    mobile nav implementation

--- src/Components/Nav/MainNavigation.jsx
+++ src/Components/Nav/MainNavigation.jsx
@@ -1,54 +1,66 @@
 import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 import { NavLink } from 'react-router-dom';
 import CartButton from '../Carts/CartButton';
 import classes from './MainNavigation.module.css';
 import menuIcon from '../../icons/icon-hamburger.svg';
 const MainNavigation = () => {
   const [showMobileNav, setShowMobileNav] = useState(false);
-  const mobileNavHandler = () => {
-    console.log('hey');
-    setShowMobileNav(true);
-  };
-
   return (
     <header>
       <nav className={classes.nav}>
         <span className={classes.logo}>Cravings</span>
 
-        <div className={classes.links}>
+        <div>
           <img
             src={menuIcon}
             alt=""
             className={classes['menu-icon']}
-            onClick={mobileNavHandler}
+            onClick={() => {
+              setShowMobileNav(!showMobileNav);
+            }}
           />
-
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive ? classes.active : undefined
+          <motion.div
+            transition={{
+              opacity: { ease: 'linear' },
+              layout: { duration: 0.3 },
+            }}
+            layout
+            className={
+              showMobileNav
+                ? classes['mobilenav']
+                : classes['nav_inner_wrapper']
             }
-            end
           >
-            Home
-          </NavLink>
-          <NavLink
-            to="/products"
-            className={({ isActive }) =>
-              isActive ? classes.active : undefined
-            }
-            end
-          >
-            Products
-          </NavLink>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/products"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Products
+            </NavLink>
+            <NavLink
+              to="./carts"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              <CartButton />
+            </NavLink>
+          </motion.div>
         </div>
-        <NavLink
-          to="./carts"
-          className={({ isActive }) => (isActive ? classes.active : undefined)}
-          end
-        >
-          <CartButton />
-        </NavLink>
       </nav>
     </header>
   );
